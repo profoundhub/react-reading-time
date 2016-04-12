6b5667e974c9943e1b555f1fefde5b9a019fb9b9
Daniel Lim <hbt@markhamboards.com>
2016-04-11 22:25:03 -0400

    few changes before new changes. Now at v.3.2.a

--- example/react-reading-time.jsx
+++ example/react-reading-time.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import ReadingTime from "../src/reading-time"
 
 class ReactReadingTime extends React.component {
   render() {
     return (
-      React.createElement('div', {className: 'container' }, 'Hello React!')
+      React.createElement("div", {className: "container"}, "Hello React!")
     );
   }
 }
 
-ReactDOM.render(<ReactReadingTime />, document.getElementById('react'));
+ReactDOM.render(<ReactReadingTime />, document.getElementById("React"));
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -30,4 +30,3 @@ module.exports = {
   ],
   devtool: "eval-source-map"
 };
-
